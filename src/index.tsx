b92fc9ad56c46fcfec6489eeedecc5e7b5fb226f
offme16 <offmemyg@gmail.com>
2024-03-10 13:37:26 +0300

    fsd

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/App';
+import './app/styles/index.scss';
+import { StoreProvider } from './app/providers/storeProvider';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <ErrorBoundary>
+            <StoreProvider>
+                <App />
+            </StoreProvider>
+        </ErrorBoundary>
+    </BrowserRouter>,
 );
-
-reportWebVitals();
